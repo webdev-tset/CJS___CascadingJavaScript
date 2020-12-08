bb5b8d84615f24017e6f36c25599dcb8a5c4e752
ach cyr <webdev.archist@gmail.com>
2020-12-08 12:55:44 +0100

    *component navbar: almost finished

---
+++ components/___template.js
@@ -0,0 +1,19 @@
+//TEMPLATE DE CREATION D'UN WEB-COMPONENT
+class MyClass extends HTMLElement{
+     constructor(){
+          super()
+     }
+     connectedCallback(){
+          
+     }
+     disconnectedCallback(){
+          
+     }
+     adoptedCallback (){
+          
+     }
+     attributeChangedCallback (){
+          
+     }
+}
+window.customElements.define('my-class', MyClass)
--- components/navbar.js
+++ components/navbar.js
@@ -1,23 +1,77 @@
 window.addEventListener('load', function(){
-    alert('ok, doc\'s ready !')
-    let i, the_component = document.getElementsByTagName('nav-bar')
-    the_component = the_component.length!==0 ? the_component : document.getElementsByClassName('navbarre')
+    // alert('ok, doc\'s ready !')
+    let i, $the_component = $('nav-bar').length
+        ? $('nav-bar')
+        : $('.navbarre'), the_component = $the_component[0]
+        
     if(the_component.length===0)return false
-    for(i in the_component)if(the_component.hasOwnProperty(i))
-        console.log(the_component[i] +" ok ");
+    $the_component.on('open',()=>{alert('oui')})
+    
+    // the_component.dataset.data = `{"anchor":{"title": "${$the_component.find('>a').text()||title}", href=}}`
+    let data = navbar_data;
 
-let anchor, button, menu, form
-    for(i in the_component)if(the_component.hasOwnProperty(i)){
-        anchor = the_component[i].querySelector(':scope>a')
-        button = the_component[i].querySelector(':scope>button')
-        menu = the_component[i].querySelector(':scope>menu')
-        form = the_component[i].querySelector(':scope>form')
-    }
+    let anchor = the_component.querySelector(':scope>a'),$anchor=$(anchor),
+    button = the_component.querySelector(':scope>button'),$button=$(button),
+    menu = the_component.querySelector(':scope>menu'),$menu=$(menu),
+    form = the_component.querySelector(':scope>form'),$form=$(form)
+
+    ;
+    $anchor.html(anchor.innerHTML || data.anchor.title)
+    $anchor.attr('href', $anchor.attr('href') || data.anchor.href)
     button.setAttribute("data-toggle", "collapse")
     button.setAttribute("data-target", "menu")
     button.setAttribute("aria-expanded", "false")
     button.setAttribute("aria-label", "Toggle navigation")
     //button ==>> PAS BESOIN DE L'ATTRIBUT 'aria-controls' => https://www.accede-web.com/notices/interface-riche/panneaux-depliants/#ancre-06-->
     button.setAttribute("aria-controls", "menu")
-
+    button.append(document.createElement('span'))
+    console.log(getTag__Menu(data.menu));
+    $menu.append(getTag__Menu(data.menu))
+    $form.append($('<input/>').attr({'type': 'submmit', 'placeholder': 'Search'}))
+        .append($('<button/>').addClass('btn btn-outline-success my-2 my-sm-0').text('Search').attr('type','submit'))
 })
+
+
+
+    function getTag__Menu(data){
+        let items = data.map(li=>$('<li/>'))
+        items.forEach((li,i)=>{
+            console.log(data[i]);
+            $(li).append(
+                $('<a/>').html(data[i].html||'Lien menu '+i).
+                attr({'href': data[i].href||'#menu-'+i})
+                .addClass('nav-link')
+            )
+            if(i==0)$(li).addClass('active')
+            if(data[i].dropdown){
+                $(li).find('>a')
+                    .addClass('dropdown-toggle')
+                    .attr({id:'navbarmenu_dropdown_'+i, 'data-toggle': 'dropdown', 'aria-haspopup': 'true', 'aria-expanded': 'false'})
+                $(li).append(
+                    $('<div class="dropdown-menu" aria-labelledby="navbarmenu_dropdown_'+i+'" />')
+                        .append(data[i].dropdown.map(
+                            dropdown=>$('<a/>').addClass('dropdown-item')
+                                .html(dropdown.html)
+                                .attr('href', dropdown.href)
+                        ))
+                )
+            }
+        })
+        return items
+    }
+
+    // function getComponent__Dropdown(){
+    //     return `TODO: 
+    //         <a href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+    //         <div aria-labelledby="dropdownId"><!--div,ul,ol,dt-->
+    //             <a href="#">Action 1</a>
+    //             <a href="#">Action 2</a>
+    //         </div
+    //     `
+    // }
+    
+    
+    
+
+
+
